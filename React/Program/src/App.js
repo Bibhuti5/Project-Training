78ca1fb61e8d4daad4301cdb9fa81296e646e9e8
Sahoo <bibhuti-bhusan.sahoo@capgemini.com>
2022-06-23 17:42:27 +0530

    Added UseEffect Hooks and Notes At 6 pm

--- React/Program/src/App.js
+++ React/Program/src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import ClassCounter from "./Hook_componets/ClassCounter";
 import HookCounter from "./Hook_componets/HookCounter";
 import HookCounterTwo from "./Hook_componets/HookCounterTwo";
-
+import ClassCounterOne from "./UseEffect/ClassCounterOne";
 function App() {
   return (
     <div className="App">
       <ClassCounter />
       <HookCounter />
       <HookCounterTwo />
+      <ClassCounterOne />
     </div>
   );
 }
